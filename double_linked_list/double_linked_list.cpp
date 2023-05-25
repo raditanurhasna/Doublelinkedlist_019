ab3d9cae3e45f5b3d720bb6e2a7a5b4a7bfe31b6
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-25 11:38:05 +0700

    Membuat perulangan dan mencari Node

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -67,11 +67,17 @@ void DoubleLinkedList::addnote() {
 		if (current->next != NULL)
 			current->next->prev = newNode;
 		current->next = newNode;
-
 		
 }
 
+	bool DoubleLinkedList::search(int rollNo, Node * *previous, Node * *current) {
+		*previous = *current = START;
+		while (*current != NULL && rollNo != (*current)->noMhs) {
+			*previous = *current;
+			*current = (*current)->next;
+		}
+		return (*current != NULL);
 
-
+	}
 
 
