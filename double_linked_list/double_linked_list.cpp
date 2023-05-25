36ecf0cb370fdd3d4a886cf72be29cc22dc92c34
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-25 16:35:23 +0700

    Digunakan untuk menjalan data sesuai dengan pilihan

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -177,6 +177,25 @@ int main() {
 			cout << "\nEnter your choice (1-6): ";
 			char ch;
 			cin >> ch;
+
+			switch (ch) {
+			case '1':
+				obj.addnote();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.ascending();
+				break;
+			case '4':
+				obj.descending();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
 			
 
 		}
