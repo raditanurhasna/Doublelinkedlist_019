1ace083fd00348bd46f2764f511ceddc5b41f5ef
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-25 11:22:25 +0700

    Membuat Duolicate number not allowed

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -45,9 +45,19 @@ void DoubleLinkedList::addnote() {
 	if (START == NULL || nim <= START->noMhs) {
 		if (START != NULL && nim == START->noMhs) {
 			cout << "\nDuplicate number not allowed" << endl;
+			return;
 		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	
+		}
+	
 	}
-
+		
 }
 
 
